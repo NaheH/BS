6a58040adc807435c38d03a2d6ec3f072d0838bf
Nahe <nahema.hutin@gmail.com>
2023-04-09 18:55:37 +0200

    commit

--- js/game.js
+++ js/game.js
@@ -27,8 +27,26 @@
         // liste des joueurs
         players: [],
 
-        // fin de jeu
-        win: false,
+        start: function(number){
+                     // défini l'ordre des phase de jeu
+                     if(number === 0){
+                        this.phaseOrder = [
+                            this.PHASE_INIT_PLAYER,
+                            this.PHASE_INIT_OPPONENT,
+                            this.PHASE_PLAY_PLAYER,
+                            this.PHASE_PLAY_OPPONENT,
+                            this.PHASE_GAME_OVER
+                        ];
+                     } else {
+                        this.phaseOrder = [
+                            this.PHASE_INIT_PLAYER,
+                            this.PHASE_INIT_OPPONENT,
+                            this.PHASE_PLAY_OPPONENT,
+                            this.PHASE_PLAY_PLAYER,
+                            this.PHASE_GAME_OVER
+                        ];
+                     }
+        },
         // lancement du jeu
         init: function () {
 
@@ -36,14 +54,7 @@
             this.grid = document.querySelector('.board .main-grid');
             this.miniGrid = document.querySelector('.mini-grid');
 
-            // défini l'ordre des phase de jeu
-            this.phaseOrder = [
-                this.PHASE_INIT_PLAYER,
-                this.PHASE_INIT_OPPONENT,
-                this.PHASE_PLAY_PLAYER,
-                this.PHASE_PLAY_OPPONENT,
-                this.PHASE_GAME_OVER
-            ];
+   
             this.playerTurnPhaseIndex = 0;
 
             // initialise les joueurs
@@ -51,7 +62,7 @@
 
             // ajoute les écouteur d'événement sur la grille
             this.addListeners();
-            console.log('debut, currentPhase : ' + this.currentPhase);
+
             // c'est parti !
             this.goNextPhase();
         },
@@ -70,45 +81,44 @@
             // récupération du numéro d'index de la phase courante
             var ci = this.phaseOrder.indexOf(this.currentPhase);
             var self = this;
-            if (this.win === true) {
-                this.currentPhase = this.phaseOrder[4];
-            } else if (ci !== this.phaseOrder.length - 1) {
+
+            if (ci !== this.phaseOrder.length - 1) {
                 this.currentPhase = this.phaseOrder[ci + 1];
+            } else {
+                this.currentPhase = this.phaseOrder[0];
             }
+
             switch (this.currentPhase) {
-                case this.PHASE_GAME_OVER:
-                    // detection de la fin de partie
-                    if (!this.gameIsOver()) {
-                        this.currentPhase = this.phaseOrder[2];
-                        utils.info("C'est à votre tour de jouer");
-                        // le jeu n'est pas terminé on recommence un tour de jeu
-                    } else {
-                        utils.info("Partie terminée");
-                    }
-                    break;
-                case this.PHASE_INIT_PLAYER:
-                    utils.info("Placez vos bateaux");
-                    break;
-                case this.PHASE_INIT_OPPONENT:
-                    this.wait();
-                    utils.info("En attente de votre adversaire");
-                    this.players[1].areShipsOk(function () {
-                        self.stopWaiting();
-                        self.goNextPhase();
-                    });
-                    break;
-                case this.PHASE_PLAY_PLAYER:
-                    utils.info("C'est à vous de jouer");
-                    break;
-                case this.PHASE_PLAY_OPPONENT:
-                    utils.info("C'est au tour de votre adversaire");
-                    this.players[1].play();
-                    break;
+            case this.PHASE_GAME_OVER:
+                // detection de la fin de partie
+                if (!this.gameIsOver()) {
+                    // le jeu n'est pas terminé on recommence un tour de jeu
+                    this.currentPhase = this.phaseOrder[this.playerTurnPhaseIndex];
+                }
+                break;
+            case this.PHASE_INIT_PLAYER:
+                utils.info("Placez vos bateaux");
+                break;
+            case this.PHASE_INIT_OPPONENT:
+                this.wait();
+                utils.info("En attente de votre adversaire");
+                this.players[1].isShipOk(function(){
+                    self.stopWaiting();
+                    self.goNextPhase();
+                });
+                break;
+            case this.PHASE_PLAY_PLAYER:
+                utils.info("A vous de jouer, choisissez une case !");
+                break;
+            case this.PHASE_PLAY_OPPONENT:
+                utils.info("A votre adversaire de jouer...");
+                this.players[1].play();
+                break;
             }
 
         },
         gameIsOver: function () {
-            return game.win;
+            return false;
         },
         getPhase: function () {
             if (this.waiting) {
@@ -128,19 +138,9 @@
             // on ajoute des acouteur uniquement sur la grid (délégation d'événement)
             this.grid.addEventListener('mousemove', _.bind(this.handleMouseMove, this));
             this.grid.addEventListener('click', _.bind(this.handleClick, this));
-            this.grid.addEventListener('contextmenu', _.bind(this.handleRightClick, this));
-        },
-        displayPreview: function (ship, e) {
-            if (ship.getDirection() === "horizontal") {
-                ship.dom.style.top = "" + (utils.eq(e.target.parentNode)) * utils.CELL_SIZE + "px";
-                ship.dom.style.left = "" + utils.eq(e.target) * utils.CELL_SIZE - Math.floor(ship.getLife() / 2) * utils.CELL_SIZE + "px";
-            } else {
-                ship.dom.style.top = "" + (utils.eq(e.target.parentNode)) * utils.CELL_SIZE - Math.floor(ship.getLife() / 2) * utils.CELL_SIZE + "px";
-                ship.dom.style.left = "" + utils.eq(e.target) * utils.CELL_SIZE + "px";
-            }
         },
         handleMouseMove: function (e) {
-            // on est dans la phase de placement des bateaux
+            // on est dans la phase de placement des bateau
             if (this.getPhase() === this.PHASE_INIT_PLAYER && e.target.classList.contains('cell')) {
                 var ship = this.players[0].fleet[this.players[0].activeShip];
 
@@ -152,64 +152,48 @@
                 }
 
                 // décalage visuelle, le point d'ancrage du curseur est au milieu du bateau
-                this.displayPreview(ship, e);
+                ship.dom.style.top = "" + (utils.eq(e.target.parentNode)) * utils.CELL_SIZE - (600 + this.players[0].activeShip * 60) + "px";
+                ship.dom.style.left = "" + utils.eq(e.target) * utils.CELL_SIZE - Math.floor(ship.getLife() / 2) * utils.CELL_SIZE + "px";
             }
         },
         handleClick: function (e) {
             // self garde une référence vers "this" en cas de changement de scope
             var self = this;
-            var ship = this.players[0].fleet[this.players[0].activeShip];
 
             // si on a cliqué sur une cellule (délégation d'événement)
             if (e.target.classList.contains('cell')) {
                 // si on est dans la phase de placement des bateau
-                if (this.getPhase() === this.PHASE_INIT_PLAYER && ship.dom.parentNode) {
+                if (this.getPhase() === this.PHASE_INIT_PLAYER) {
                     // on enregistre la position du bateau, si cela se passe bien (la fonction renvoie true) on continue
                     if (this.players[0].setActiveShipPosition(utils.eq(e.target), utils.eq(e.target.parentNode))) {
                         // et on passe au bateau suivant (si il n'y en plus la fonction retournera false)
                         if (!this.players[0].activateNextShip()) {
                             this.wait();
-                            utils.confirm("Confirmer ?", function () {
+                            utils.confirm("Confirmez le placement ?", function () {
                                 // si le placement est confirmé
                                 self.stopWaiting();
                                 self.renderMiniMap();
                                 self.players[0].clearPreview();
-                                // console.log(self.players[0].grid);
                                 self.goNextPhase();
-
                             }, function () {
                                 self.stopWaiting();
                                 // sinon, on efface les bateaux (les positions enregistrées), et on recommence
                                 self.players[0].resetShipPlacement();
-
                             });
                         }
                     }
-                    // si on est dans la phase de jeu (du joueur humain)
+                // si on est dans la phase de jeu (du joueur humain)
                 } else if (this.getPhase() === this.PHASE_PLAY_PLAYER) {
                     this.players[0].play(utils.eq(e.target), utils.eq(e.target.parentNode));
                 }
-            }
-        },
-        handleRightClick: function (e) {
-            if (this.getPhase() === this.PHASE_INIT_PLAYER) {
-                var ship = this.players[0].fleet[this.players[0].activeShip];
-                if (ship.getDirection() === "horizontal") {
-                    ship.setDirection("vertical");
-                    ship.dom.style.height = "" + utils.CELL_SIZE * ship.life + "px";
-                    ship.dom.style.width = "" + utils.CELL_SIZE + "px";
-                } else {
-                    ship.setDirection("horizontal");
-                    ship.dom.style.height = "" + utils.CELL_SIZE + "px";
-                    ship.dom.style.width = "" + utils.CELL_SIZE * ship.life + "px";
-                }
-                this.displayPreview(ship, e);
             }
         },
         // fonction utlisée par les objets représentant les joueurs (ordinateur ou non)
         // pour placer un tir et obtenir de l'adversaire l'information de réusssite ou non du tir
         fire: function (from, col, line, callback) {
             this.wait();
+            const audiio = new Audio ('../sound/hitsmtg.mp3');
+            audiio.play();
             var self = this;
             var msg = "";
 
@@ -218,39 +202,25 @@
                 ? this.players[1]
                 : this.players[0];
 
-            if (this.currentPhase === this.PHASE_PLAY_PLAYER) {
-                msg += "Vous avez ";
-            }
             if (this.currentPhase === this.PHASE_PLAY_OPPONENT) {
-                msg += "Votre adversaire vous a ";
+                msg += "Votre adversaire vous a... ";
             }
 
             // on demande à l'attaqué si il a un bateaux à la position visée
             // le résultat devra être passé en paramètre à la fonction de callback (3e paramètre)
             target.receiveAttack(col, line, function (hasSucceed) {
 
-                if (hasSucceed === 'alreadyHit') {
-                    msg += "déja touché...";
-                } else if (hasSucceed === 'alreadyMiss') {
-                    msg += "déja raté...";
-                } else if (hasSucceed === 'sunk') {
-                    msg += "touché coulé !";
-                } else if (hasSucceed) {
-                    msg += "touché !";
+                if (hasSucceed) {
+                    msg += "Touché !";
                 } else {
-                    msg += "manqué";
+                    msg += "Manqué...";
                 }
+
                 utils.info(msg);
 
                 // on invoque la fonction callback (4e paramètre passé à la méthode fire)
                 // pour transmettre à l'attaquant le résultat de l'attaque
                 callback(hasSucceed);
-                self.renderMap();
-                if (self.currentPhase === self.PHASE_PLAY_OPPONENT) {
-                    self.players[0].renderShips(self.miniGrid, self.players[1].tries);
-                    console.log(self.players[0].fleet);
-                    console.log(self.players[1].fleet);
-                }
 
                 // on fait une petite pause avant de continuer...
                 // histoire de laisser le temps au joueur de lire les message affiché
@@ -264,16 +234,49 @@
         renderMap: function () {
             this.players[0].renderTries(this.grid);
         },
+        // ex 2
         renderMiniMap: function () {
-            this.players[0].renderMiniMap(this);
+            self = this;
+            player.fleet.forEach(function(e){
+               let clone = e.dom.cloneNode();
+               self.miniGrid.appendChild(clone);
+            });
         }
     };
+
     // point d'entrée
     document.addEventListener('DOMContentLoaded', function () {
-        document.addEventListener('contextmenu', function (event) {
-            event.preventDefault();
-        }, true);
-        game.init();
+        var display = document.querySelector('.game-info');
+        var select = document.createElement('select');
+        var ops1, ops2, ops3, defaut;
+        defaut = document.createElement('option');
+        defaut.textContent = "Who starts?";
+        ops1 = document.createElement('option');
+        ops1.textContent = "Le joueur commence";
+        ops1.setAttribute("value","0");
+        ops2 = document.createElement('option');
+        ops2.textContent = "L'ordinateur commence";
+        ops2.setAttribute("value","1");
+        ops3 = document.createElement('option');
+        ops3.textContent = "Randomize";
+        ops3.setAttribute("value","2");
+        select.appendChild(defaut);
+        select.appendChild(ops1);
+        select.appendChild(ops2);
+        select.appendChild(ops3);
+        display.appendChild(select);
+        
+        document.querySelector("select").onchange = ()=>{
+            var rand;
+            if(display.querySelector('select').value === "2"){
+                rand = Math.floor(Math.random()*10)%2
+            } else {
+                rand = display.querySelector('select').value;
+            }
+            game.start(parseInt(rand));
+            game.init();
+        };
+        
     });
 
 }());
--- js/player.js
+++ js/player.js
@@ -4,7 +4,7 @@
 (function (global) {
     "use strict";
 
-    //var ship = {dom: {parentNode: {removeChild: function () {}}}};
+    var ship = {dom: {parentNode: {removeChild: function () {}}}};
 
     var player = {
         grid: [],
@@ -12,9 +12,8 @@
         fleet: [],
         game: null,
         activeShip: 0,
-        setGame: function (game) {
-            this.game = game;
-        },
+        setGame: function(game){
+        this.game = game},
         init: function () {
             // créé la flotte
             this.fleet.push(shipFactory.build(shipFactory.TYPE_BATTLESHIP));
@@ -35,87 +34,39 @@
         // quand il est attaqué le joueur doit dire si il a un bateaux ou non à l'emplacement choisi par l'adversaire
         receiveAttack: function (col, line, callback) {
             var succeed = false;
-            var cell = this.grid[line][col];
-            if (cell === 'x') {
-                succeed = "alreadyHit";
-            } else if (cell === '.') {
-                succeed = "alreadyMiss";
-            } else if (cell !== 0) {
+            
+            var target = this.game.players.indexOf(this) === 0
+            ? this.game.players[1]
+            : this.game.players[0];
+
+            if (this.grid[line][col] !== 0 && this.grid[line][col] !== false) {
                 succeed = true;
-                console.log("cell : " + cell);
-                if (cell > this.fleet.length) {
-                    cell -= this.fleet.length;
-                }
-                console.log("cell -- : " + cell);
-                this.fleet[cell - 1].life--;
-                this.grid[line][col] = 'x';
-                if (this.fleet[cell - 1].life === 0) {
-                    succeed = 'sunk';
-                    if (this.game.players[0].fleet[cell - 1].life === 0) {
-                        var sunkShip = document.querySelector('.' + this.fleet[cell - 1].getName());
-                        sunkShip.className += " sunk";
-                    }
-                    if (this.checkGameOver() === true) {
-                        console.log("game Over : true");
-                        this.game.win = true;
-                    }
-                }
-            } else {
-                this.grid[line][col] = '.';
+                this.grid[line][col] = 0;
             }
+            // ajout png  et classe
+            var png = (succeed === true) ? "toucher": "rater"; 
+            if(target === this.game.players[1]){
+                this.game.grid.querySelector(`.row:nth-child(${line+1}) .cell:nth-child(${col+1})`).classList.add(png);
+            }
+
+            //ajout sound effect
+            const successhot = (succeed === true)? new Audio("../sound/success.mp3") : new Audio("../sound/flop.mp3");
+            setTimeout(() => {successhot.play()}, 1000);
             callback.call(undefined, succeed);
-        },
-        checkGameOver: function () {
-            var shipsAlives = this.fleet.length;
-            this.fleet.forEach(function (ship) {
-                console.log("ship id : " + ship.getId() + " hp : " + ship.getLife());
-                if (ship.getLife() <= 0) {
-                    shipsAlives--;
-                }
-                console.log("vrai");
-            });
-            if (shipsAlives === 0) {
-                return true;
-            } else {
-                return false;
-            }
         },
         setActiveShipPosition: function (x, y) {
             var ship = this.fleet[this.activeShip];
             var i = 0;
-            var j = 0;
-            var math = Math.floor(ship.getLife() / 2);
 
-
-            if (ship.getDirection() === "horizontal") {
-                while (j < ship.getLife()) {
-                    if (this.grid[y][x - math + j] !== 0) {
-                        return false;
-                    }
-                    j++;
+            while (i < ship.getLife()) {
+                if (this.grid[y][x + i] !== 0) {
+                    return  false;
                 }
-                while (i < ship.getLife()) {
-                    this.grid[y][x - math + i] = ship.getId();
-                    i += 1;
-                }
-                console.debug(ship);
-                return true;
-            } else {
-                while (j < ship.getLife()) {
-                    if (typeof (this.grid[y - math + j]) === 'undefined')
-                        return false;
-                    if (this.grid[y - math + j][x] !== 0) {
-                        return false;
-                    }
-                    j++;
-                }
-                while (i < ship.getLife()) {
-                    this.grid[y - math + i][x] = ship.getId();
-                    i += 1;
-                }
-                console.debug(ship);
-                return true;
+                this.grid[y][x + i] = ship.getId();
+                i += 1;
             }
+
+            return true;
         },
         clearPreview: function () {
             this.fleet.forEach(function (ship) {
@@ -141,37 +92,18 @@
         renderTries: function (grid) {
             this.tries.forEach(function (row, rid) {
                 row.forEach(function (val, col) {
-
                     var node = grid.querySelector('.row:nth-child(' + (rid + 1) + ') .cell:nth-child(' + (col + 1) + ')');
 
-                    if (val === true || val === 'sunk') {
-                        node.style.backgroundColor = '#cd424c';
+                    if (val === true) {
+                        node.style.backgroundColor = '#e60019';
                     } else if (val === false) {
-                        node.style.backgroundColor = '#666666';
+                        node.style.backgroundColor = '#aeaeae';
                     }
                 });
             });
         },
-        renderShips: function (grid, tries) {
-            tries.forEach(function (row, rid) {
-                row.forEach(function (val, col) {
-                    var node = grid.querySelector('.row:nth-child(' + (col + 1) + ') .cell:nth-child(' + (rid + 1) + ')');
-
-                    if (val === true || val === 'sunk') {
-                        node.style.backgroundColor = '#e2a130';
-                    }
-                    // else if (val === false) {
-                    //     node.style.backgroundColor = '#aeaeae';
-                    // }
-                });
-            });
-        },
-        renderMiniMap: function (game) {
-            var fleet = game.players[0].fleet;
-            var minigrid = game.miniGrid;
-            fleet.forEach(function (ship) {
-                minigrid.innerHTML += ship.dom.outerHTML;
-            });
+        renderShips: function (grid) {
+            
         }
     };
 
